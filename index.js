0860b650f4ed1efdc4f9b4ae7e2cdc085661515c
Huan Ji <huan@transcriptic.com>
2018-02-27 11:15:25 -0800

    export asyncGenerators

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 export fetchJSON from './fetchJSON.js';
-export asyncAction from './asyncRedux.js';
-export asyncReducer from './asyncRedux.js';
+export { asyncAction, asyncReducer, asyncGenerators } from './asyncRedux.js';
